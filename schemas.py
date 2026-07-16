6add8dfc3d57d986b409d3fcfe038a6e76ce88b2
Ronnie Spencer <ronnie.spencer23@kzoo.edu>
2026-07-15 20:07:00 -0400

    Started working on authorization. Updated User Model, updated classes in schemas. and created a config.py file

--- schemas.py
+++ schemas.py
@@ -8,22 +8,28 @@ class UserBase(BaseModel): # whats shared between UserCreate and UserResponse
     email: EmailStr = Field(max_length=120)
 
 class UserCreate(UserBase):
-    pass
+    password:str = Field(min_length=8) # password is not stored in db, but we need it to create a user
     
 
-class UserResponse(UserBase):
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
     id: int
+    username: str
     image_file: str | None
     image_path: str # property on user model
+    
+class UserPrivate(UserPublic):
+    email: EmailStr
     
 class UserUpdate(BaseModel): # whats shared between UserCreate and UserResponse
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
     
-    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 class PostBase(BaseModel):   # shared between returning and creating a post
@@ -47,4 +53,4 @@ class PostResponse(PostBase):  # PostResponse inherits title, content and author
     id: int
     user_id: int
     date_posted: datetime
-    author: UserResponse
+    author: UserPublic # we want to return the author of the post, but not their password or email. We can use UserPublic for that.
